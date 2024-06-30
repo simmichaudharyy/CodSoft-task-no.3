b6de3a30bf8477605687811b4618d1d128c2e3bf
simmichaudharyy <simmichaudhary577@gmail.com>
2024-06-30 21:58:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons = document.querySelectorAll ('.button') ;
+let array = Array.from(buttons);
+array.forEach((button) => {
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML== '='){
+            string= eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = ""
+            document.querySelector('input').value = string
+        }
+
+        else{        
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector ('input').value = string;
+        }
+
+   
+    })
+}) 
